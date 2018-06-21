e1c00610f74fe45ca113f9e866f03b1d66ff2ced
Kolya Stocks <kolyamstocks@gmail.com>
2018-06-21 17:51:53 +0000

    more js tutorials

--- Javascript/test.js
+++ Javascript/test.js
@@ -30,3 +30,59 @@ function Invoice(subTotal) {
     }, 2000);
 }
 const inv = Invoice(200);
+
+
+// Swapping variables
+let playerOne = "Guy";
+let playerTwo = "Girl";
+
+[playerOne, playerTwo] = [playerTwo, playerOne];
+
+console.log(`
+Player one: ${playerOne}
+Player two: ${playerTwo}
+`);
+
+
+// Array destructuring
+const apiList = [
+    '1st link',
+    '2nd link',
+    '3rd link'
+    ]
+    
+const [posts, repos, profile] = apiList;
+console.log(posts);
+console.log(repos);
+console.log(profile);
+
+
+// cool implementation
+const user = {
+    name: 'Kolya',
+    email: 'guy@website.com'
+}
+
+const renderUser = ({name, email}) => {
+    console.log(`${name}: ${email}`);
+}
+
+renderUser(user);
+
+
+// spread operator
+
+//combining arrays
+let shoppingCart = [345, 375, 234, 123];
+let newItems = [98,123];
+
+shoppingCart.push(...newItems);
+console.log(shoppingCart); // [ 345, 375, 234, 123, 98, 123 ]
+
+// copying arrays
+const shoppingCart2 = [345, 375, 234, 123];
+const updatedCart = shoppingCart.slice();
+updatedCart.push(5);
+
+console.log(updatedCart);
+console.log(shoppingCart2);
