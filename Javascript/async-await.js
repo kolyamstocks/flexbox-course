a2854c2ad3a57e7e45ea737307e3cf9c00c7fc95
Kolya Stocks <kolyamstocks@gmail.com>
2018-06-26 01:08:22 +0000

    async fin

--- Javascript/async-await.js
+++ Javascript/async-await.js
@@ -1,8 +1,10 @@
+// Learning
+/*
 const login = () => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve('User logged in...');
-        }, 2000);
+        }, 4000);
     });
 };
 
@@ -13,3 +15,39 @@ const updateAccount = () => {
         }, 2000);
     });
 };
+
+async function loginActivities(login, updateAccount) {
+    const returnedLogin = await login;
+    console.log(returnedLogin);
+
+    const returnedUpdateAccount = await updateAccount;
+    console.log(returnedUpdateAccount);
+}
+
+loginActivities(login(), updateAccount());
+*/
+
+// Real Application
+// https://api.dailysmarty.com/posts
+// https://api.github.com/users/kolyamstocks/repos
+async function queryApis() {
+  try {
+  const postsPromise = fetch('https://api.dailysmarty.com/posts');
+  const posts = await postsPromise.then(res => res.json());
+  console.log(posts);
+  } catch(err) {
+    console.log(err);
+    console.log('There was an error with the DailySmarty API');
+  }
+  
+  try {
+  const reposPromise = fetch('https://api.github.com/users/kolyamstocks/repos');
+  const repos = await reposPromise.then(res => res.json());
+  console.log(repos);
+  } catch(err) {
+    console.log(err);
+    console.log('There was an error with the GitHub API');
+  }
+}
+
+queryApis();
